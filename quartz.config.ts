dfb8f296fac3fb8c609881a9bb5985505f6ca0b8
Felix Nie <hongtuo.nie@u.nus.edu>
2025-04-11 17:01:59 +0800

    Added figure centering and captioning

--- quartz.config.ts
+++ quartz.config.ts
@@ -75,6 +75,7 @@ const config: QuartzConfig = {
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
+      Plugin.FigureCaptions(),
       Plugin.Citations({
         bibliographyFile: "./content/bibliography.bib",
         suppressBibliography: false,
