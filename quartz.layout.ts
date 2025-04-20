2f1101e3497a9171db8afbc4476a0f6f9d788b8d
Felix Nie <hongtuo.nie@u.nus.edu>
2025-04-20 15:36:48 +0800

    Added official read mode with new icon

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -108,6 +108,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
+    Component.ReaderMode(),
     Component.Explorer({
       folderDefaultState: "open",
       folderClickBehavior: "collapse",
@@ -132,6 +133,7 @@ export const defaultListPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
+    Component.ReaderMode(),
     Component.Explorer({
       folderDefaultState: "open",
       folderClickBehavior: "collapse",
